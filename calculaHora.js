f44be4a25db83836b64dbfee30c2f11e1420de5d
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2013-01-31 18:42:47 -0400

    Adicionado mensagens
    Adicionado verificação de expediente e mostrando mensagem de acordo.

--- calculaHora.js
+++ calculaHora.js
@@ -3,11 +3,17 @@
 		almoco = document.getElementById("almoco"),
 		retorno = document.getElementById("retorno"),
 		expediente = document.getElementById("expediente"),
+		saida = document.getElementById("saida"),
+		mensagem = document.getElementById("mensagem"),
 		horaEntrada,
 		horaAlmoco,
 		horaRetorno,
+		horaSaida,
+		diferencaExpediente,
 		minutoExpediente,
 		stringExpediente;
+
+	mensagem.innerHTML = "";
 
 	if (entrada.value.trim() === "" || almoco.value.trim() === "" || retorno.value.trim() === "") {
 		expediente.value = "";
@@ -24,10 +30,27 @@
 	stringExpediente = ("00" + Math.floor(minutoExpediente / 60)).slice(-2) + ":" + ("00" + (minutoExpediente % 60)).slice(-2);
 
 	expediente.value = stringExpediente;
+
+	if (saida.value.trim() === "") {
+		return true;
+	}
+
+	horaSaida = saida.valueAsNumber / 60000;
+
+	diferencaExpediente = horaSaida - minutoExpediente;
+
+	if (diferencaExpediente === 0) {
+		mensagem.innerHTML = "<h1>Expediente correto</h1><span>Você até pode fingir, mas não acredito!</span>";
+	} else if (diferencaExpediente > 0) {
+		mensagem.innerHTML = "<h1>Expediente extraordinário</h1><span>Huum! Você é muito trabalhadeiro.</span>";
+	} else {
+		mensagem.innerHTML = "<h1>Saída antecipada</h1><span>Vai trabalhar vagabundo!</span>";
+	}
 };
 
 window.onload = function () {
 	document.getElementById("entrada").onblur = atualiza;
 	document.getElementById("almoco").onblur = atualiza;
 	document.getElementById("retorno").onblur = atualiza;
+	document.getElementById("saida").onblur = atualiza;
 };
